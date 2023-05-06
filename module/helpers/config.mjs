64e2951ebbdca0fcc44df15b6734570b1ef4e868
superseva <superseva@gmail.com>
2023-05-06 18:13:59 -0500

    initial

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const OUTGUNNED = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ OUTGUNNED.abilities = {
+  "str": "OUTGUNNED.AbilityStr",
+  "dex": "OUTGUNNED.AbilityDex",
+  "con": "OUTGUNNED.AbilityCon",
+  "int": "OUTGUNNED.AbilityInt",
+  "wis": "OUTGUNNED.AbilityWis",
+  "cha": "OUTGUNNED.AbilityCha"
+};
+
+OUTGUNNED.abilityAbbreviations = {
+  "str": "OUTGUNNED.AbilityStrAbbr",
+  "dex": "OUTGUNNED.AbilityDexAbbr",
+  "con": "OUTGUNNED.AbilityConAbbr",
+  "int": "OUTGUNNED.AbilityIntAbbr",
+  "wis": "OUTGUNNED.AbilityWisAbbr",
+  "cha": "OUTGUNNED.AbilityChaAbbr"
+};
